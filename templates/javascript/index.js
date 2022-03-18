ace75f05c8153aa7d826ce8f41a9429ca03f41e7
James Martinez <jmar910@gmail.com>
2022-03-18 14:41:46 -0400

    Update template app with CJS module format

--- templates/javascript/index.js
+++ templates/javascript/index.js
@@ -1,7 +1,5 @@
-const App = class App {
-  async run(Turbine) {
+exports.App = class App {
+  async run(turbine) {
     // TODO: Write out instructional code examples here
   }
 };
-
-export { App };
