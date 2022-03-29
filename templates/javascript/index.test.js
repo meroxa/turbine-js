2eb58db26cee19662b65ab4a055aa6f76bd74e61
justmisosoup <sara@meroxa.io>
2022-03-29 16:28:42 -0700

    Adjust typo and add additional line around using any framework of choice.

--- templates/javascript/index.test.js
+++ templates/javascript/index.test.js
@@ -1,6 +1,7 @@
 const index = require('./index');
 
-// This unit test example uses the Jest, a JavaScript testing framework (https://jestjs.io/)
+// This unit test example uses Jest, a JavaScript testing framework (https://jestjs.io/)
+// However, you may use any testing framework of your choice
 // To learn more about how to use this testing framework
 // Refer to the Jest documentation https://jestjs.io/docs/getting-started
 // To run this unit test, use `yarn test` or `npm run test`
