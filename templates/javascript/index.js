1be1dce06481079bf88439a91e30e50b5a7e8cf1
justmisosoup <sara@meroxa.io>
2022-03-29 14:39:14 -0700

    Add jest unit test example

--- templates/javascript/index.js
+++ templates/javascript/index.js
@@ -1,5 +1,49 @@
+const stringHash = require("string-hash");
+
+function iAmHelping(str) {
+  return `~~~${str}~~~`;
+}
+
+exports.Anonymize = function Anonymize(records) {
+  records.forEach((record) => {
+    record.value.payload.after.email = iAmHelping(
+      stringHash(record.value.payload.after.email).toString()
+    );
+  });
+
+  return records;
+};
+
 exports.App = class App {
   async run(turbine) {
-    // TODO: Write out instructional code examples here
+    // To configure your data stores as resources on the Meroxa Platform
+    // use the Meroxa Dashboard, CLI, or Meroxa Terraform Provider
+    // For more details refer to: http://docs.meroxa.com/
+
+    // Identify an upstream data store for your data app
+    // with the `resources` function
+    // Replace `source_name` with the resource name the
+    // data store was configured with on Meroxa
+    let source = await turbine.resources("source_name");
+
+    // Specify which upstream records to pull
+    // with the `reocrds` function
+    // Replace `collection_name` with a table, collection,
+    // or bucket name in your data store
+    let records = await source.records("collection_name");
+
+    // Specify what code to execute against upstream records
+    // with the `process` function
+    // Replace `anonymized` with the name of your function code
+    let func = await turbine.process(records, exports.Anonymize);
+
+    // Identify the downstream data store for your data app
+    // with the `resources` function
+    // Replace ` destination_name` with the resource name the
+    // data store was configured with on Meroxa
+    let destination = await turbine.resources("destination_name");
+
+    // Create destination connector with function output as input
+    await destination.write(records, "collection_name");
   }
 };
---
+++ templates/javascript/index.test.js
@@ -0,0 +1,10 @@
+const index = require('./index');
+
+// This unit test example uses the Jest, a JavaScript testing framework (https://jestjs.io/)
+// To learn more about how to use this testing framework
+// Refer to the Jest documentation https://jestjs.io/docs/getting-started
+// To run this unit test, use `yarn test` or `npm run test`
+
+test('Test Anonymize function', () => {
+    expect(exports.Anonymize).toEqual(undefined);
+});
