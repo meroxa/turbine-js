c94bed78588fa7b9599691d48346e03b25fb0b31
James Martinez <jmar910@gmail.com>
2022-08-12 05:30:07 -0400

    Fix test coverage for monorepo

--- packages/turbine-framework/scripts/test-coverage.sh
+++
@@ -1,7 +0,0 @@
-mkdir .nyc_output
-mkdir nyc-coverage-report
-yarn run test:coverage
-yarn run test:coverage-report:json
-SUMMARY=$(cat nyc-coverage-report/coverage-summary.json)
-RESULTS=$(node -pe 'JSON.parse(process.argv[1]).total.lines.pct' "$SUMMARY")
-echo "Total Test Coverage: $RESULTS% ✨"
