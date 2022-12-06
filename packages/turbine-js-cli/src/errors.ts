77b8eaba2cd91754cba1973c483d696b01973727
Janelle Tavares <janelletavares@users.noreply.github.com>
2022-12-06 10:46:07 -0800

    Print error details in a readable way if present (#226)
    * Print error details in a readable way if present
    
    * prettier
    
    * Update error output with test
    
    Co-authored-by: Brett Goulder <brett.goulder@gmail.com>

--- packages/turbine-js-cli/src/errors.ts
+++ packages/turbine-js-cli/src/errors.ts
@@ -11,7 +11,7 @@ export class BaseError extends Error {
   }
 
   unwrapMessage(): string {
-    if (!this.wrappedError) {
+    if (!this.wrappedError || !this.wrappedError.message) {
       return this.message;
     }
 
@@ -36,7 +36,6 @@ export class APIError extends BaseError {
   unwrapMessage(): string {
     let unwrapped = super.unwrapMessage();
     const wrappedError = this.wrappedError as AxiosError;
-
     const method = wrappedError.config.method!.toUpperCase();
     const url = wrappedError.config.baseURL! + wrappedError.config.url!;
 
@@ -44,6 +43,19 @@ export class APIError extends BaseError {
 
     if (wrappedError.response?.data.message) {
       unwrapped = `${unwrapped} : ${wrappedError.response?.data.message}`;
+
+      let details = wrappedError.response?.data.details;
+      if (details.length > 0) {
+        let report = `; ${details.length} detail(s) provided\n`;
+        let count = 1;
+        for (let [key, value] of details) {
+          let joined = value.join(". ");
+
+          report = report + `${count++}. ${key}: ${joined}.\n`;
+        }
+
+        unwrapped = `${unwrapped} ${report}`;
+      }
     }
 
     return unwrapped;
